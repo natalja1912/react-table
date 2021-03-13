8a86e94b437f2774d0cea08c97a8befb8d28840f
natalja1912@gmail.com <natalja1912@gmail.com>
2021-03-13 19:44:10 +0300

    Updates

---
+++ precache-manifest.c0b7ca3489903cb800fd86eb3e76a767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebaf57aabc090267cfe734f3483a0fd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bfa58acc70526fed09a",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "31cb22b64dd17120ff45",
+    "url": "./static/css/main.a0b00630.chunk.css"
+  },
+  {
+    "revision": "7bfa58acc70526fed09a",
+    "url": "./static/js/2.557fe200.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.557fe200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31cb22b64dd17120ff45",
+    "url": "./static/js/main.724ca962.chunk.js"
+  },
+  {
+    "revision": "82bfb7ed32e775882e51",
+    "url": "./static/js/runtime-main.dcd2432a.js"
+  }
+]);
